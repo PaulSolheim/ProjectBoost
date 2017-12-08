31dba86bdd64d6e61e5e13c9956ed414649a8ffe
Pål Grønstad Solheim <paul.solheim@gmail.com>
2017-12-08 07:33:59 +0100

    added Starfighter
    added Starfighter patrol ship to level 1.

---
+++ Assets/Starfighter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Starfighter : MonoBehaviour
+{
+    [SerializeField] float mainThrust = 100f;
+
+    Rigidbody rigidBody;
+    bool rotateOnLeft = true;
+    bool rotateOnRight = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangePosition();
+    }
+
+    private void ChangePosition()
+    {
+        if (transform.position.x < -15f)
+        {
+            if (rotateOnLeft)
+            {
+                Rotate();
+            }
+        }
+        else if (transform.position.x > 15f)
+        {
+            if (rotateOnRight)
+            {
+                Rotate();
+            }
+        }
+        float thrustThisFrame = mainThrust * Time.deltaTime;
+        rigidBody.AddRelativeForce(Vector3.left * thrustThisFrame);
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true;                // take manual control of rotation
+        transform.Rotate(Vector3.up, 180f);
+        rigidBody.freezeRotation = false;               // resume physics control of rotation
+        rotateOnLeft = !rotateOnLeft;
+        rotateOnRight = !rotateOnRight;
+    }
+}
